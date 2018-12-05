56717adeeb1155a5384092ad0b8891e4b2d5e34d
= <sharperm@163.com>
2018-12-05 11:12:09 +0800

    [add]添加实现文件

--- source/MySQLLib/ParamBuffer.cpp
+++ source/MySQLLib/ParamBuffer.cpp
@@ -114,10 +114,36 @@ namespace MYSQLDB
 	
 	ParamBuffer::ParamBuffer(const unsigned short int i)
 	{
-		_buffer = new void *[sizeof(short int)];
-		_bufferSize = sizeof(short int);
+		_buffer = new void *[sizeof(unsigned short int)];
+		_bufferSize = sizeof(unsigned short int);
 		_bufferLength = _bufferSize;
 		_type = MYSQL_TYPE_SHORT;
+		_isNull = false;
+		_isUnsigned = true;
+		_isTruncated = false;
+
+		*((unsigned short int *)_buffer) = i;
+	}
+	
+	ParamBuffer::ParamBuffer(const int i)
+	{
+		_buffer = new void *[sizeof( int)];
+		_bufferSize = sizeof( int);
+		_bufferLength = _bufferSize;
+		_type = MYSQL_TYPE_LONG;
+		_isNull = false;
+		_isUnsigned = false;
+		_isTruncated = false;
+
+		*((int *)_buffer) = i;
+	}
+	
+	ParamBuffer::ParamBuffer(const unsigned int i)
+	{
+		_buffer = new void *[sizeof(unsigned int)];
+		_bufferSize = sizeof(unsigned int);
+		_bufferLength = _bufferSize;
+		_type = MYSQL_TYPE_LONG;
 		_isNull = false;
 		_isUnsigned = true;
 		_isTruncated = false;
@@ -125,38 +151,138 @@ namespace MYSQLDB
 		*((unsigned int *)_buffer) = i;
 	}
 	
-	ParamBuffer::ParamBuffer(const int i)
-	{
-
-	}
-	
-	ParamBuffer::ParamBuffer(const unsigned int i)
-	{
-
-	}
-	
 	ParamBuffer::ParamBuffer(const Binary &data)
 	{
+		_buffer = new void *[sizeof(Binary)];
+		_bufferSize = data.bufferSize();
+		_bufferLength = _bufferSize;
+		_type = MYSQL_TYPE_BLOB;
+		_isNull = false;
+		_isUnsigned = false;
+		_isTruncated = false;
 
+		memcpy(_buffer, data.buffer(), sizeof(Binary));
 	}
 	
 	ParamBuffer::ParamBuffer(const char ch)
 	{
+		_bufferSize = sizeof(char);
+		_buffer = new void* [sizeof(_bufferSize)];
+		_bufferLength = _bufferSize;
+		_type = MYSQL_TYPE_TINY;
+		_isNull = false;
+		_isTruncated = false;
+		_isUnsigned = false;
 
+		*((char *)_buffer) = ch;
 	}
 	
 	ParamBuffer::ParamBuffer(const unsigned char ch)
 	{
+		_bufferSize = sizeof(char);
+		_buffer = new void*[sizeof(_bufferSize)];
+		_bufferLength = _bufferSize;
+		_type = MYSQL_TYPE_TINY;
+		_isNull = false;
+		_isTruncated = false;
+		_isUnsigned = false;
 
+		*((char *)_buffer) = ch;
 	}
 	
 	ParamBuffer::ParamBuffer(const float f)
 	{
+		_buffer = new void*[(sizeof(float))];
+		_bufferSize = sizeof(float);
+		_bufferLength = _bufferSize;
+		_type = MYSQL_TYPE_FLOAT;
+		_isNull = false;
+		_isTruncated = false;
+		_isUnsigned = true;
 
+		*((float *)_buffer) = f;
 	}
 	
 	ParamBuffer::ParamBuffer(const double d)
 	{
+		_buffer = new void*[(sizeof(double))];
+		_bufferSize = sizeof(double);
+		_bufferLength = _bufferSize;
+		_type = MYSQL_TYPE_DOUBLE;
+		_isNull = false;
+		_isTruncated = false;
+		_isUnsigned = true;
 
+		*((double *)_buffer) = d;
+	}
+
+	ParamBuffer::ParamBuffer(const ParamBuffer &copy)
+	{
+		
+	}
+
+	ParamBuffer::~ParamBuffer()
+	{
+		if (_buffer)
+		{
+			delete _buffer;
+			_buffer = nullptr;
+		}
+		_bufferSize = 0;
+		_bufferLength = 0;
+		_isNull = true;
+	}
+
+	void ParamBuffer::resizeBlob(size_t newSize)
+	{
+		if (_type != MYSQL_TYPE_BLOB) 
+		{
+			throw DatabaseException("Error in ParamBuffer::ResizeBlob", 0, "----", "Datatype is not blob");
+		}
+
+		if (_bufferSize != newSize) 
+		{
+			free(_buffer);
+			_buffer = new void* [newSize];
+			_bufferSize = newSize;
+			_bufferLength = 0;
+		}
+	}
+
+	void *ParamBuffer::buffer() const
+	{
+		return _buffer;
+	}
+
+	enum_field_types ParamBuffer::bufferType() const
+	{
+		return _type;
+	}
+
+	size_t ParamBuffer::bufferSize() const
+	{
+		return _bufferSize;
+	}
+
+	unsigned long *ParamBuffer::bufferLength()
+	{
+		return &_bufferLength;
+	}
+
+	bool ParamBuffer::isNull()
+	{
+		return _isNull;
+	}
+	bool ParamBuffer::isUnsigned()
+	{
+		return _isUnsigned;
+	}
+	bool ParamBuffer::isTruncated()
+	{
+		return _isTruncated;
+	}
+	bool ParamBuffer::error()
+	{
+		return _errno;
 	}
 }
